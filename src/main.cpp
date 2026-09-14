5550812925077cf37a531a8c26037afa6ebf6aeb
zj19356239680 <zj19356239680@163.com>
2026-09-14 20:41:02 +0800

    验证 Boost.Asio io_context 基础运行

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <boost/asio/io_context.hpp>
 
 int main()
 {
     std::cout << "ApiGate bootstrap OK\n";
+
+    boost::asio::io_context io;
+    std::cout << io.run() << std::endl;
+
     return 0;
 }
