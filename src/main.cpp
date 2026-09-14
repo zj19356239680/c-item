8e9d423c158fed8a7fe111d9908993fe2f0a2ff8
zj19356239680 <zj19356239680@163.com>
2026-09-14 21:22:17 +0800

    验证 Asio post 回调任务调度

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/post.hpp>
+
+void print_task()
+{
+    std::cout << "run task\n";
+}
 
 int main()
 {
     std::cout << "ApiGate bootstrap OK\n";
 
     boost::asio::io_context io;
+    boost::asio::post(io, print_task);
     std::cout << io.run() << std::endl;
 
     return 0;
